0f92358f2f0db23798b5d8b9eac5b9f85d5ef7e9
Henning Heinold <heinold@inf.fu-berlin.de>
2010-01-16 01:04:36 +0100

    * fix direct source modification

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ wireless network card's capabilities using the Linux Wireless Extensions.""",
     url = "http://pythonwifi.wikispot.org/",
     packages = ['pythonwifi'],
 
-    data_files=[('', ['README']),
+    data_files=[('', ['README', 'INSTALL']),
                 ('examples', ['examples/iwlist.py', 'examples/iwconfig.py']),
                 ('docs', ['docs/AUTHORS', 'docs/BUGS', 'docs/LICENSE.GPL',
                           'docs/LICENSE.LGPL', 'docs/NEWS', 'docs/ROADMAP',
